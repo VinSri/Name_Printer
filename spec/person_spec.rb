7507b836070ab1bf7bf7c70160edbc8b2d04e7b6
Vino Sri <vinosrik@gmail.com>
2017-02-12 12:42:37 +0000

    Refactored the rspec

--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,32 +2,42 @@ describe Person do
 
   subject { described_class }
   describe '#initialize' do
-    it "initilizes with first name" do
-      expect( subject.new("John", "Doe").first_name ).to eq "John"
+    context 'when initialized with first_name and last_name' do
+      subject { described_class.new("John","Doe")}
+      specify { expect(subject.first_name).to eq "John" }
+      specify { expect(subject.last_name).to eq "Doe" }
     end
 
-    it "initializes with last name" do
-      expect( subject.new("John","Doe").last_name ).to eq "Doe"
+    context 'when initialized with only first name' do
+      subject { described_class.new("John")}
+      specify { expect(subject.first_name).to eq "John" }
+    end
+
+    context 'when initialized with only last name' do
+      subject { described_class.new( nil,"Doe")}
+      specify { expect(subject.last_name).to eq "Doe" }
+    end
+
+    context 'when initialized without first_name and last name' do
+      subject { described_class.new}
+      specify { expect(subject.first_name).to eq nil }
+      specify { expect(subject.last_name).to eq nil }
     end
   end
 
   describe '#print_name' do
-    it "prints the full name" do
-      expect( subject.new("John","Smith").print_name ).to eq "John Smith"
+    context 'when first_name and last_name are passed as parameter' do
+      specify { expect( subject.new("John","Smith").print_name ).to eq "John Smith" }
     end
 
-    it "prints the full name" do
+    context 'when first_name and last_name are assigned  seperately' do
       p1 = Person.new
       p1.first_name = 'Jane'
       p1.last_name = 'Doe'
-      expect( p1.print_name ).to eq "Jane Doe"
+      specify { expect( p1.print_name ).to eq "Jane Doe" }
     end
 
-    it "prints the full name with first name and last name capitalized" do
-      p1 = Person.new
-      p1.first_name = 'jane'
-      p1.last_name = 'doe'
-      expect( p1.print_name ).to eq "Jane Doe"
-    end
+
+
   end
 end
