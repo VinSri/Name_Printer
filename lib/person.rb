7d4d1548d642d40e67c94b00abdc4eff85f066d9
Vino Sri <vinosrik@gmail.com>
2017-02-12 11:46:35 +0000

    Added print_name method to the person class

--- lib/person.rb
+++ lib/person.rb
@@ -1,8 +1,16 @@
 class Person
 
-  attr_accessor :first_name
+  attr_accessor :first_name, :last_name
 
-  def initialize first_name
+  def initialize first_name , last_name 
     @first_name = first_name
+    @last_name = last_name
   end
+
+  def print_name
+    first_name + " " + last_name
+  end
+
+
 end
+
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -2,11 +2,17 @@ describe Person do
 
   describe '#initialize' do
     it "initilizes with first name" do
-      expect( Person.new("John").first_name ).to eq "John"
+      expect( Person.new("John", "Doe").first_name ).to eq "John"
     end
 
     it "initializes with last name" do
-      expect( Person.new("Doe").last_name ).to eq "Doe"
+      expect( Person.new("John","Doe").last_name ).to eq "Doe"
+    end
+  end
+
+  describe '#print_name' do
+    it "prints the full name" do
+      expect( Person.new("John","Smith").print_name ).to eq "John Smith"
     end
   end
 end
