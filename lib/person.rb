fcad097e61f85bae51bf73f4f5dab5067346f4c7
Vino Sri <vinosrik@gmail.com>
2017-02-12 11:54:56 +0000

    Modified the initialize class

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,7 @@ class Person
 
   attr_accessor :first_name, :last_name
 
-  def initialize first_name , last_name 
+  def initialize first_name = nil, last_name = nil
     @first_name = first_name
     @last_name = last_name
   end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -14,5 +14,12 @@ describe Person do
     it "prints the full name" do
       expect( Person.new("John","Smith").print_name ).to eq "John Smith"
     end
+
+    it "prints the full name" do
+      p1 = Person.new
+      p1.first_name = 'Jane'
+      p1.last_name = 'Doe'
+      expect( p1.print_name ).to eq "Jane Doe"
+    end
   end
 end
